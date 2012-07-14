4ffe66c7d17e3588bc54d30959079ea0f6f85714
Steven Yan <1398683@163.com>
2012-07-14 13:12:43 +0800

    init env

---
+++ cocos2dx_example/proj.android/src/org/cocos2dx/example/cocos2dx_example.java
@@ -0,0 +1,15 @@
+package org.cocos2dx.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class cocos2dx_example extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
